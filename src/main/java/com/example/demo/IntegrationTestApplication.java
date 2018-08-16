90c69f18c629d6143e86f9c2136f04b01c71f4e6
Thiago Melo <tmelo@gcosi.local>
2018-08-16 14:11:40 -0400

    changing code to work with message template

--- src/main/java/com/example/demo/IntegrationTestApplication.java
+++ src/main/java/com/example/demo/IntegrationTestApplication.java
@@ -9,10 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
 
 @SpringBootApplication
 @Configuration
@@ -23,28 +22,21 @@ public class IntegrationTestApplication implements ApplicationRunner{
 	@Qualifier("channel1In")
 	private DirectChannel directInChannel;
 	
-	@Autowired
-	@Qualifier("channel1Out")
-	private DirectChannel directOutChannel;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(IntegrationTestApplication.class, args);
 	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		
-		directOutChannel.subscribe(new MessageHandler() {
-			
-			@Override
-			public void handleMessage(Message<?> message) throws MessagingException {
-				System.out.println(message.getPayload());
-				
-			}
-		});
-		
+
 		Message<String> message = MessageBuilder.withPayload("Message created successfully!").setHeader("header1", "Teste header").build(); 
-		directInChannel.send(message);
+
+		MessagingTemplate template = new MessagingTemplate();
+		
+		Message<?> returnedMessage = template.sendAndReceive(directInChannel, message);
+		
+		System.out.println(returnedMessage.getPayload());
+		
 	}
 	
 	
